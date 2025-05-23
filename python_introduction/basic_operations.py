1ea99d71e3fc9d51e1c9ccc38650ce9df7f9fe9b
Magnus Edu <magnusedu5@gmail.com>
2025-05-23 19:28:32 +0100

    Added a file with arithmetic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+diff = number1 - number2
+mul = number1 * number2
+
+print('Addition of 10 and 5 is', sum)
+print('Subtraction of 10 and 5 is', diff)
+print('Multiplication of 10 and 5 is', mul)
