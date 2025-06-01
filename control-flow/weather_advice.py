88faeabaa7f54a9b3ec7b9548e7960e9c91085a1
Magnus Edu <magnusedu5@gmail.com>
2025-06-01 23:24:44 +0100

    program to Utilize conditional statements to guide program execution based on user input regarding weather conditions.

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,10 +1,10 @@
 whether = input("What's the weather like today? (sunny/rainy/cold):")
 
-if whether == 'sunny':
+if whether == "sunny":
     print('wear a t-shirt and sunglasses.')
-elif whether == 'rainy':
+elif whether == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif whether == 'cold':
+elif whether == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
