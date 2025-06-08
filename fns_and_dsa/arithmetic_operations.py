3acb2b26c9a6a77561cd0ab3c6ed2ba8526d1ba6
Magnus Edu <magnusedu5@gmail.com>
2025-06-08 22:00:22 +0100

    practicing importing modules

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,15 @@
 def perform_operation(num1, num2, operation):
     if operation == 'add':
-        print (num1 + num2)
+        return num1 + num2
     elif operation == 'subtract':
-        print(num1 - num2)
+        return num1 - num2
     elif operation == 'multiply':
-        print(num1 * num2)
+        return num1 * num2
     elif operation == 'divide':
         if num2 == 0:
-            print("Can't perform this operation")
+            return "Can't perform this operation"
         else:
-            print(num1 / num2)
+            return num1 / num2
     else:
         print("Error")
 
