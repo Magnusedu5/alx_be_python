c319df983423e33b729aca39c7852d0bb1030f6e
Magnus Edu <magnusedu5@gmail.com>
2025-06-10 14:43:30 +0100

    Program to print future dates

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
     return formatted_date
 
 def calculate_future_date():
-    days_to_add = int(input("Enter the number of days to add to the Current date: "))
+    days_to_add = int(input("Enter the number of days to add to the current date: "))
     current_date = datetime.now()
     future_date = current_date + timedelta(days=days_to_add)
     print(f"Future date: {future_date.strftime("%Y-%m-%d")}")
