b5ad7c87036b0907386eaea8f34df4ca302690f9
Magnus Edu <magnusedu5@gmail.com>
2025-06-15 15:45:03 +0100

    Created a simple bank account class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,12 +5,14 @@ class BankAccount:
 
     def deposit(self, amount):
         self.account_balance += amount
-        return self.account_balance
 
 
     def withdraw(self, amount):
-        self.account_balance -= amount
-        return self.account_balance
+        if amount > self.account_balance:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
 
 
     def display_balance(self):
